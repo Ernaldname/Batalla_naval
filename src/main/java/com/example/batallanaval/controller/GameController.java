806cbe1457cffa120846e6f80d5b459c106229ff
Edinson Ramirez <edinsonr547@gmail.com>
2024-06-04 22:55:35 -0500

    UPDATE MODEL, VIEW AND CONTROLLER

--- src/main/java/com/example/batallanaval/controller/GameController.java
+++ src/main/java/com/example/batallanaval/controller/GameController.java
@@ -1,5 +1,24 @@
 package com.example.batallanaval.controller;
 
+import com.example.batallanaval.view.alert.AlertBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import com.example.batallanaval.model.Player;
+import com.example.batallanaval.view.GameStage;
+import com.example.batallanaval.view.WelcomeStage;
+
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 public class GameController {
+    @FXML
+    private TextField campoDeTextoPalabraClave;
+
+    @FXML
+    void onHandleButtonPlay(ActionEvent event) throws IOException {
+        // Codigo de la accion por el boton
+    }
 
 }
--- src/main/java/com/example/batallanaval/model/Player.java
+++ src/main/java/com/example/batallanaval/model/Player.java
@@ -1,16 +1,5 @@
 package com.example.batallanaval.model;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+public class Player {
 
-public class Player extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
 }
